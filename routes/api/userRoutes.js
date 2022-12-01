dd1329bae7bd1f80f33b09a3e15abfc277617358
Beth DeCarlo <110702442+bethdecarlo@users.noreply.github.com>
2022-11-30 20:47:12 -0500

    added API route for /api/users to userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,6 +2,8 @@
 //GET all users
 //GET all users by its _id and populated thought and friend data
 //POST a new user
+//POST to add a new friend from user's friend list
+// DELETE to remove a friend from a user's friend list
 
 const router = require('express').Router();
 const {
